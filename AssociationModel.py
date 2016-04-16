cc8c10e951d438bdc6ae4714150519ff93e6d3dc
Arkadiusz Chrobak <ac93912@st.amu.ede.pl>
2016-04-16 12:24:15 +0200

    code clean

--- AssociationModel.py
+++ AssociationModel.py
@@ -3,14 +3,15 @@ def printmodel(F,n):
     line = ''
     for i in range(n):
         if (i % 5 == 0) and (i != 0):
-            print line+"\n"
+            print(line + "\n")
             line =''
         if F[i] == -1:
             line += " - "
         else:
             line += " * "
         if (i+1 == n):
-            print line+"\n"
+            print(line + "\n")
+
 
 def sgn(FZ,W,Z1,n):
     wynik = 0.0
@@ -60,9 +61,9 @@ Z1z = [-1.0, -1.0, 1.0, -1.0, -1.0,
         -1.0, -1.0, 1.0, -1.0, -1.0,
         -1.0, -1.0, 1.0, -1.0, -1.0]
 
-print "Z0:"+"\n"
+print("Z0:" + "\n")
 printmodel(Z0,n)
-print "Z1:"+"\n"
+print("Z1:" + "\n")
 printmodel(Z1,n)
 
 
@@ -75,30 +76,29 @@ FZ1z = [ 0.0 for i in range(n) ]
 # SGN
 multiplyvector(W,Z0,Z1,n)
 
-print "\n\n SGN(W*Z0) = \n"
+print("\n\n SGN(W*Z0) = \n")
 
 sgn(FZ0,W,Z0,n)
 printmodel(FZ0,n)
 
-print "\n\n SGN(W*Z1) = \n"
+print("\n\n SGN(W*Z1) = \n")
 
 sgn(FZ1,W,Z1,n)
 printmodel(FZ1,n)
 
-
-print "Zaburzenie 0 ':"+"\n"
+print("Zaburzenie 0 ':" + "\n")
 printmodel(Z0z,n)
-print "Zaburzenie 1':"+"\n"
+print("Zaburzenie 1':" + "\n")
 printmodel(Z1z,n)
 
 multiplyvector(W1,Z0z,Z1z,n)
 
-print "\n\n SGN(W * Z0' - Zaburzone = \n"
+print("\n\n SGN(W * Z0' - Zaburzone = \n")
 
 sgn(FZ0z,W,Z0z,n)
 printmodel(FZ0z,n)
 
-print "\n\n SGN(W * Z1' - Zaburzone  = \n"
+print("\n\n SGN(W * Z1' - Zaburzone  = \n")
 
 sgn(FZ1z,W,Z1z,n)
 printmodel(FZ1z,n)
